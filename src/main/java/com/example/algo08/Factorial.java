b67760ded9f381d6e294082b44d852021d3b1161
SO YEON LEE <thdus1323@gmail.com>
2024-04-27 19:36:08 +0900

    팩토리얼 예제

--- src/main/java/com/example/algo08/Factorial.java
+++ src/main/java/com/example/algo08/Factorial.java
@@ -7,26 +7,6 @@ public class Factorial {
     public static void main(String[] args) {
 
         int inputNum = 7;
-
-        //1로 초기화(0하면 무조건 0이니까)
-        //누적숫자
-        int accNum = 1;
-        // 5! = 5*4*3*2*1
-        // 7! = 7*6*5*4*3*2*1
-
-        //방법1 (1->5)
-        for(int i =1; i <=inputNum; i++){
-            accNum *= i; //accNum = accNum * i;
-        }
-        System.out.println(accNum);
-
-        //방법2 (5->1)
-        accNum = 1;
-        for (int i = inputNum ; i>=1; i--){
-            accNum *= i; // accNum = accNum *i;
-        }
-
-        System.out.println(accNum);
     }
 
 }
