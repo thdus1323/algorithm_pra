898fcf22f406b122226db2c6124d4e0374ba0ae9
SO YEON LEE <thdus1323@gmail.com>
2024-04-27 19:17:03 +0900

    소수 정리 완

--- src/main/java/com/example/demo/Main_stu.java
+++ src/main/java/com/example/demo/Main_stu.java
@@ -2,6 +2,6 @@ package com.example.demo;
 
 public class Main_stu {
     public static void main(String[] args) {
-        Student st1 = new Student()
+        Student st1 = new Student();
     }
 }
